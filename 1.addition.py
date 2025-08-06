f0d5031d769b9eb0418d0205b793d1297a1005b6
chevitichandu <chevitichandu1@gmail.com>
2025-08-06 16:41:52 +0530

    Add files via upload

--- 1.addition.py
+++ 1.addition.py
@@ -2,3 +2,27 @@ a=10
 b=20
 print("addition:",a+b)
 ##addition: 30
+
+
+a=5
+b=10
+c=a+b
+print("Addition:",c)
+#Addition: 15
+
+a=100
+b=200
+print("addition:",a+b)
+#addition: 300
+
+a=30
+b=40
+c=a+b
+print(c)
+#70
+
+
+a=8
+b=26
+print(a+b)
+#34
