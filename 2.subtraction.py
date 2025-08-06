95f9d48ca05a3cd45ca3131f746dc39fbed9d066
chevitichandu <chevitichandu1@gmail.com>
2025-08-06 16:40:37 +0530

    Add files via upload

---
+++ 2.subtraction.py
@@ -0,0 +1,25 @@
+a=7
+b=3
+c=a-b
+print("subtraction:",c)
+
+#subtraction: 4
+
+a=12
+b=6
+c=a-b
+print(c)
+#6
+
+a=150
+b=75
+print("subtraction:",a-b)
+
+#subtraction: 75
+
+
+a=26
+b=8
+print(a-b)
+
+#18
